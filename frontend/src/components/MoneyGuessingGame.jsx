82b2019f811c22d158c3d615148851833d381ace
Andre Silva <andrenations@gmail.com>
2024-12-23 22:47:05 -0500

    clean code

--- frontend/src/components/MoneyGuessingGame.jsx
+++ frontend/src/components/MoneyGuessingGame.jsx
@@ -89,8 +89,6 @@ const moneyGuessingGame = () => {
           </div>
         </div>
 
-        <div></div>
-
         {gameOver && (
           <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center text-center p-4 z-10 bg-black bg-opacity-70 rounded-lg">
             {endOfTheGame()}
